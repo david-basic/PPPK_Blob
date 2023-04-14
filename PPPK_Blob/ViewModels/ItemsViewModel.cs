eaf9e6840a067706cb8f892f82e67867034a6d11
David Bašić <d.basic97.db@gmail.com>
2023-04-15 00:56:21 +0200

    work on Item VM

--- PPPK_Blob/ViewModels/ItemsViewModel.cs
+++ PPPK_Blob/ViewModels/ItemsViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Azure.Storage.Blobs.Models;
+using PPPK_Blob.Dao;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,7 @@ namespace PPPK_Blob.ViewModels
 {
     internal class ItemsViewModel
     {
-        public const string ForwardSlash = "/";
+        public const string FORWARD_SLASH = "/";
 
         public ObservableCollection<string> Directories { get; }
         public ObservableCollection<BlobItem> Items { get; }
@@ -23,6 +25,7 @@ namespace PPPK_Blob.ViewModels
                 Refresh();
             }
         }
+        private string directory;
 
         public ItemsViewModel()
         {
@@ -33,10 +36,61 @@ namespace PPPK_Blob.ViewModels
 
         private void Refresh()
         {
+            Items.Clear();
+            Directories.Clear();
 
+            Repository.Container.GetBlobs().ToList().ForEach(item =>
+            {
+                if (item.Name.Contains(FORWARD_SLASH))
+                {
+                    string dir = item.Name.Substring(0, item.Name.LastIndexOf(FORWARD_SLASH));
+
+                    if (!Directories.Contains(dir))
+                    {
+                        Directories.Add(dir);
+                    }
+                }
+
+                if (string.IsNullOrEmpty(Directory) && !item.Name.Contains(FORWARD_SLASH))
+                {
+                    Items.Add(item);
+                }
+                else if (!string.IsNullOrEmpty(Directory) && item.Name.StartsWith($"{Directory}{FORWARD_SLASH}"))
+                {
+                    Items.Add(item);
+                }
+            });
         }
 
-        private string directory;
+        public async Task UploadAsync(string path, string dir)
+        {
+            string filename = path.Substring(path.LastIndexOf(Path.DirectorySeparatorChar) + 1);
 
+            if (!string.IsNullOrEmpty(dir))
+            {
+                filename = $"{dir}{FORWARD_SLASH}{filename}";
+            }
+
+            using (var fs = File.OpenRead(path))
+            {
+                await Repository.Container.GetBlobClient(filename).UploadAsync(fs, true);
+            }
+            Refresh();
+        }
+
+        public async Task DownloadAsync(BlobItem item, string path)
+        {
+            using (var fs = File.OpenWrite(path))
+            {
+                await Repository.Container.GetBlobClient(item.Name).DownloadToAsync(fs);
+            }
+            Refresh();
+        }
+
+        public async Task DeleteAsync(BlobItem item)
+        {
+            await Repository.Container.GetBlobClient(item.Name).DeleteAsync();
+            Refresh();
+        }
     }
 }
